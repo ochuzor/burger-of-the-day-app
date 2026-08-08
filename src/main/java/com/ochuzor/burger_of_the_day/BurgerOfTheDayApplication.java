c2de7ef6fa391e8d484a101d970442300920ae9f
Chinedu Eze <ochuzor@gmail.com>
2026-08-08 21:10:22 +0300

    init

---
+++ src/main/java/com/ochuzor/burger_of_the_day/BurgerOfTheDayApplication.java
@@ -0,0 +1,13 @@
+package com.ochuzor.burger_of_the_day;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerOfTheDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurgerOfTheDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ochuzor/burger_of_the_day/BurgerOfTheDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ochuzor.burger_of_the_day;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerOfTheDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
